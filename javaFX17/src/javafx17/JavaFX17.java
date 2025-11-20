44a4486807be3e275d4de9d1d14d64590f1753ba
cajlo83 <cee.mm96@gmail.com>
2025-11-20 17:04:03 -0300

    configuracion general para trabajar en javaFX 17

---
+++ javaFX17/src/javafx17/JavaFX17.java
@@ -0,0 +1,29 @@
+
+package javafx17;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+/**
+ *
+ * @author Usuario
+ */
+public class JavaFX17 extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("hellofx.fxml"));
+        Scene scene = new Scene(root);
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
